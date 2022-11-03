53656f8d14a9972bd8ced8562d49c116a5212c7f
Luis Felipe <202makertv@gmail.com>
2022-11-03 11:12:16 -0300

    Now the bot say how many messages were sent

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from time import sleep
  
 MSG=input('Qual é a mensagem? ')
 QNT=int(input('Quantas mensagens? '))
+current_quantity = 0
 print("Por favor, coloque o cursor no campo de enviar a mensagem do telegram.")
 print("O bot vai começar a colar a mensagem '",MSG,"', ",QNT," vezes, em 3 segundos")
 sleep(3)
 for TIMES in range(QNT):
+    current_quantity += 1
+    print("CURRENT QUANTITY: ", current_quantity)
     pyautogui.write(MSG)
     pyautogui.hotkey('enter')
